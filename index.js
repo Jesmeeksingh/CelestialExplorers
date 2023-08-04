a596c1aafd846c53538d5fefdd97fdc581ccd3a5
Jesmeeksingh <141353728+Jesmeeksingh@users.noreply.github.com>
2023-08-04 20:39:17 +0530

    Update index.js
    UPDATE 1:
    NOW WE CAN SEE THE TITLE OF PICTURE AND SOME INFORMATION RELATED TO IT

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ let explore = document.querySelector('.explore-btn');
 explore.addEventListener('click',()=>{
     
     let rightimg=document.querySelector('.right-section-image');
+    let heading = document.querySelector('.heading-left');
+    let discription = document.querySelector('.about-image');
+    heading.textContent='';
+    discription.textContent='';
     rightimg.remove();
-    
+
+
+    //image
     let newrightimg = document.createElement('div');
     newrightimg.classList.add('right-section-image');
     right.appendChild(newrightimg);
@@ -19,10 +25,21 @@ explore.addEventListener('click',()=>{
     request.onload = function(){
         if(request.status===200){
         let data = JSON.parse(request.responseText);
+        let name = data.title;
+        console.log(name);
+        let explanation =data.explanation;
         let imgurl = data.hdurl;
         let image =document.createElement('img');
+        let title = document.createElement('h2');
+        let para = document.createElement('p');
+        console.log(explanation);
+        title.textContent=name;
         image.src=imgurl;
+        para.textContent=explanation;
         newrightimg.appendChild(image);
+        heading.appendChild(title);
+        discription.appendChild(para);
+        
         }
         else{
             alert('error');
