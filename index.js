e595680fd4e55dc8098bb9db0e4534c0ef6d2a83
Jesmeeksingh <141353728+Jesmeeksingh@users.noreply.github.com>
2023-08-06 14:33:58 +0530

    Update index.js
    update 2.0
    used fetch api instead of using xmlhttprequest 
    and it is more clean and readable code

--- index.js
+++ index.js
@@ -1,48 +1,39 @@
-const apikey='ShJ5lrFi2JDMZ0DSaeYUaO6wFfwBi2ZXuYrTe7U1';
-const url='https://api.nasa.gov/planetary/apod?';
-let right =document.querySelector('.right-section');
-let explore = document.querySelector('.explore-btn');
-explore.addEventListener('click',()=>{
-    
-    let rightimg=document.querySelector('.right-section-image');
-    let heading = document.querySelector('.heading-left');
-    let discription = document.querySelector('.about-image');
-    heading.textContent='';
-    discription.textContent='';
-    rightimg.remove();
+const apikey = "ShJ5lrFi2JDMZ0DSaeYUaO6wFfwBi2ZXuYrTe7U1";
+const url = "https://api.nasa.gov/planetary/apod?";
+let right = document.querySelector(".right-section");
+let explore = document.querySelector(".explore-btn");
+explore.addEventListener("click",()=>{
+  let rightimg = document.querySelector(".right-section-image");
+  let heading = document.querySelector(".heading-left");
+  let discription = document.querySelector(".about-image");
+  
+  heading.textContent = "";
+  discription.textContent = "";
+  rightimg.innerHTML = "";
 
-
-    //image
-    let newrightimg = document.createElement('div');
-    newrightimg.classList.add('right-section-image');
-    right.appendChild(newrightimg);
-
-    let dateinp = document.querySelector('.right-section input');
-    let date =dateinp.value;
-    let request = new XMLHttpRequest();
-    request.open('GET',url+'date='+date+'&api_key='+apikey,true);
-    request.send();
-    request.onload = function(){
-        if(request.status===200){
-        let data = JSON.parse(request.responseText);
-        let name = data.title;
-        console.log(name);
-        let explanation =data.explanation;
-        let imgurl = data.hdurl;
-        let image =document.createElement('img');
-        let title = document.createElement('h2');
-        let para = document.createElement('p');
-        console.log(explanation);
-        title.textContent=name;
-        image.src=imgurl;
-        para.textContent=explanation;
-        newrightimg.appendChild(image);
-        heading.appendChild(title);
-        discription.appendChild(para);
-        
-        }
-        else{
-            alert('error');
-        }
+  let dateinp = document.querySelector(".right-section input");
+  let date = dateinp.value;
+  fetch(url+"date="+date+"&api_key="+apikey)
+   .then(response =>{
+    if(!response.ok){
+      throw new Error("error occured man !!");
     }
-}) 
+    return response.json();
+   })
+   .then(data=>{
+    let imgurl = data.hdurl;
+    let explanation = data.explanation;
+    let name = data.title;
+    
+    heading.textContent = name;
+    discription.textContent=explanation
+    
+    let image = document.createElement("img");
+    image.src = imgurl;
+    rightimg.appendChild(image);
+   })
+   .catch(error =>{
+    alert("error occured :(" , error.message);
+   });
+
+})
